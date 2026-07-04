fa08dbaaafea23fb6b7c04bf6529371cabaedb24
srimathi-art <srimathipalanisami392@gmail.com>
2026-07-04 10:56:25 +0530

    Add data preprocessing

---
+++ train.py
@@ -0,0 +1,53 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, SimpleRNN, Dense
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# 1. Training sentences
+sentences = [
+    "i love machine learning",
+    "i love deep learning",
+    "machine learning is interesting",
+    "deep learning is powerful",
+    "artificial intelligence is growing",
+    "i want to learn ai",
+    "ai can predict words",
+    "rnn can learn sequences",
+    "machine learning can predict results",
+    "deep learning can generate sentences"
+]
+
+# 2. Convert words to numbers
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(sentences)
+
+word_index = tokenizer.word_index
+total_words = len(word_index) + 1
+
+print(word_index)
+print("Total words:", total_words)
+
+# 3. Create word-level training data
+input_sequences = []
+
+for sentence in sentences:
+    token_list = tokenizer.texts_to_sequences([sentence])[0]
+
+    for i in range(1, len(token_list)):
+        sequence = token_list[:i+1]
+        input_sequences.append(sequence)
+
+# 4. Make all sequences same length
+max_sequence_len = max(len(seq) for seq in input_sequences)
+
+input_sequences = pad_sequences(
+    input_sequences,
+    maxlen=max_sequence_len,
+    padding="pre"
+)
+
+# 5. Split input and target
+X_train = input_sequences[:, :-1]
+y_train = input_sequences[:, -1]
